2ed13191b623e02ca99c30af4e40752b97ba1dfb
abhmul <abhmul@gmail.com>
2016-03-25 01:10:40 -0500

    Updated to continuously get every hour

--- traffic/bing_connect.py
+++ traffic/bing_connect.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import ast
+import time
 import urllib2
 
 key = '5lr03uc8ED3BKCehrM4s~UeZYhEuwi5E-XmN3wnP7Ew~AuVG-mN8njenB8rQfdOR28vfuFYqAm7BB56b6l5MNKAld_cYwrkD32bjSvPa4yV2'
@@ -24,12 +25,17 @@ def strip_u(orig):
 #Houston Bounding Box
 bb = [29.522261,-95.645041,29.999123,-95.09572]
 
-incidents_orig = query_incidents(bb, key)
-incidents = strip_u(incidents_orig)
+
+# incidents = strip_u(incidents_orig)
 # incidents = str(incidents_orig).replace('u\'','\'' )
 # incidents = ast.literal_eval(incidents)
-print incidents
-print type(incidents)
-
-with open('incidents.json', 'w') as fp:
-    json.dump(incidents_orig, fp)
+# print incidents
+# print type(incidents)
+while(True):
+    incidents_orig = query_incidents(bb, key)
+    millis = int(round(time.time() * 1000))
+    name = 'incidentcase_bing_'+str(millis)+'.json'
+    with open(name, 'w') as fp:
+        json.dump(incidents_orig, fp)
+    print "Bing Incident File Written: " + name
+    time.sleep(3600)
